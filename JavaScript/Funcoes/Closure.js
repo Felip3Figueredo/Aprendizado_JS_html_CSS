a3ebabfc91248b5c54283b799d3d547217ee4c34
Felipe Figueredo Dos Santos Aguiar <felipefigueredo1208@gmail.com>
2023-06-14 23:52:38 -0300

    Closures

---
+++ JavaScript/Funcoes/Closure.js
@@ -0,0 +1,18 @@
+// Closure é o escopo criado quando uma função é declarada
+// Esse escopo permite que a função acesse e manipule variáveis externas à função
+
+// Contexto léxico em ação!
+
+const x = 'Global'
+
+function fora() {
+    const x = 'Local'
+    function dentro(){
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
+
